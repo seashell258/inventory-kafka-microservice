eff88aec3bcbc200c07a8f7f7b019b849ea348d0
kuanwei <kuanwei.dev@gmail.com>
2025-11-13 23:04:15 +0800

    temp

--- kafka-consumer/src/main/java/com/seashell/kafka_consumer/consumer/InventoryConsumer.java
+++ kafka-consumer/src/main/java/com/seashell/kafka_consumer/consumer/InventoryConsumer.java
@@ -1,6 +1,12 @@
 package com.seashell.kafka_consumer.consumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Service;
+
 import com.seashell.kafka_consumer.dto.InventoryBatchMessageDto;
 import com.seashell.kafka_consumer.dto.InventoryMessageDto;
 import com.seashell.kafka_consumer.exception.InvalidDtoException;
@@ -11,25 +17,18 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.stereotype.Service;
-
-import java.util.Set;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class InventoryConsumer {
     private final InventoryTransactionService inventoryTransactionService;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
     private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
-    @KafkaListener(topics = "inventory", groupId = "inventory-consumer-group")
-    public void listen(Object message, Acknowledgment ack) throws Exception {
-        System.out.println(message);
-        InventoryMessageDto dto = objectMapper.convertValue(message, InventoryMessageDto.class);
+    @KafkaListener(topics = "inventory", 
+    groupId = "inventory-consumer-group",
+    containerFactory = "singleConsumerListenerFactory" )
+    public void listen(InventoryMessageDto dto, Acknowledgment ack) throws Exception {
+        System.out.println(dto);
 
         // DTO 驗證
         Set<ConstraintViolation<InventoryMessageDto>> violations = validator.validate(dto);
@@ -51,12 +50,12 @@ public class InventoryConsumer {
 
     }
 
-    @KafkaListener(topics = "batch-inventory", groupId = "inventory-consumer-group")
-    public void listenBatch(String message, Acknowledgment ack) throws Exception {
+    @KafkaListener(topics = "batch-inventory"
+    , groupId = "inventory-consumer-group"
+    ,containerFactory = "batchConsumerListenerFactory")
+    public void listenBatch(InventoryBatchMessageDto batchDto, Acknowledgment ack) throws Exception {
 
-        System.out.println("Received: " + message);
-        // JSON -> DTO
-        InventoryBatchMessageDto batchDto = objectMapper.readValue(message, InventoryBatchMessageDto.class);
+        System.out.println("Received: " );
         // DTO 驗證
         Set<ConstraintViolation<InventoryBatchMessageDto>> violations = validator.validate(batchDto);
         if (!violations.isEmpty()) {
